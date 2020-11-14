11f96976e1b50a9551063db45adf2b825e6cf220
Renato Campos Mauro <renato@DellMini.casa>
2020-11-13 23:35:37 -0300

    Esboço acesso PCDAS

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Client } = require('@elastic/elasticsearch');
+
+const pcdas_node = "https://dados-pcdas.icict.fiocruz.br";
+const pcdas_username = "aluno.treinamento";
+const pcdas_password = "aluno123456";
+const consulta01 = {
+    index: 'datasus-sinasc-dss',
+    body: {
+        query: {
+            match_all: {}
+        }
+    }
+};
+
+// Cria objeto que representa a conexão com o serviço Elastic Search
+const client = new Client({ 
+    node: pcdas_node,
+    auth: {
+        username: pcdas_username,
+        password: pcdas_password
+    }
+});
+
+client
+    .search(consulta01)
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+;
+git 
